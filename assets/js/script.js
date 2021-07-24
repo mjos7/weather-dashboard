9e6088c8329d4b03a8379595c81f80443932cc78
mjos7 <markjnjoseph@gmail.com>
2021-07-23 17:16:36 -0700

    complete populate data within html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // 'use strict';
 
 // GLOBAL VARIABLES
+var now = dayjs();
+var today = $('.date').append(now.format('dddd, MMMM D, YYYY'));
 var searchBarEl = document.getElementById('search-bar');
 var curWeather = [{ temp: '', wind: '', humidity: '', uv: '' }];
 var day2 = [{ temp: '', wind: '', humidity: '' }];
@@ -8,17 +10,38 @@ var day3 = [{ temp: '', wind: '', humidity: '' }];
 var day4 = [{ temp: '', wind: '', humidity: '' }];
 var day5 = [{ temp: '', wind: '', humidity: '' }];
 var day6 = [{ temp: '', wind: '', humidity: '' }];
-var recentCities = [];
 
 $('#search-form').submit(function (event) {
   event.preventDefault();
   var city = searchBarEl.value.trim();
-
+  if (city == '') {
+    alert('Please enter a city');
+    return;
+  } else {
+    getCurWeather(city);
+  }
   // clear old content
   $(document).ready(function () {
     $(searchBarEl).val('');
   });
 });
+
+var setLocal = function () {
+  // 1. "Get list"
+  var recentCities = JSON.parse(localStorage.getItem('city')) || [];
+
+  recentCities.push({ city: city });
+  // 2: "Filter"
+  recentCities.filter((item, index) => recentCities.indexOf(item) === index);
+  // 3: "Reduce"
+  recentCities.reduce(
+    (unique, item) => (unique.includes(item) ? unique : [...unique, item]),
+    []
+  );
+  // 4: "Set item"
+  localStorage.setItem('city', JSON.stringify(recentCities));
+  console.log(recentCities);
+};
 
 // API URLS
 var curApiUrl = 'https://api.openweathermap.org/data/2.5/weather?';
@@ -30,16 +53,16 @@ var forApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?';
 var apiKey = '&APPID=6c4166b2aa40bda720c59188b206ddb3';
 
 var getCurWeather = function (city) {
-  fetch(`${curApiUrl}q=${city}${apiKey}`)
+  fetch(`${curApiUrl}q=${city}&units=imperial${apiKey}`)
     .then(function (response) {
       // request was successful
       if (response.ok) {
         response.json().then(function (data) {
-          console.log(data);
+          // console.log('Current Weather');
+          // console.log(data);
           getUvi(data.coord.lat, data.coord.lon);
           getForecast(city);
-
-          // CALL FORECAST API FUNCTION AND PASS CITY NAME
+          displayCurWeather(data);
         });
       } else {
         alert(`Error: City Not Found`);
@@ -50,14 +73,14 @@ var getCurWeather = function (city) {
     });
 };
 
-getCurWeather('Vancouver');
-
 var getUvi = function (lat, lon) {
   fetch(`${oneApiUrl}lat=${lat}&lon=${lon}${apiKey}`).then(function (response) {
     response
       .json()
       .then(function (data) {
-        console.log(data);
+        displayCurUvi(data);
+        // console.log('UVI');
+        // console.log(data);
       })
       .catch(function (error) {
         alert(`Unable to connect to One Call API`);
@@ -66,17 +89,78 @@ var getUvi = function (lat, lon) {
 };
 
 var getForecast = function (city) {
-  fetch(`${forApiUrl}q=${city}${apiKey}`).then(function (response) {
+  fetch(`${forApiUrl}q=${city}&units=imperial${apiKey}`).then(function (
+    response
+  ) {
     response
       .json()
       .then(function (data) {
-        console.log(data);
+        displayForecast(data);
       })
       .catch(function (error) {
         alert(`Unable to connect to 5 Day Forecast API`);
       });
   });
 };
+
+// Display Current Weather
+var displayCurWeather = function (data) {
+  document.getElementById('city-name').textContent = data.name;
+  document.getElementById('cur-temp').textContent = data.main.temp + ' ˚F';
+  document.getElementById('cur-wind').textContent = data.wind.speed + ' MPH';
+  document.getElementById('cur-humidity').textContent =
+    data.main.humidity + ' %';
+};
+
+// Display Current UVI
+var displayCurUvi = function (data) {
+  var curUviEl = document.getElementById('cur-uvi');
+  curUviEl.innerHTML = data.current.uvi;
+  curUviEl.classList.add('uv-background');
+};
+
+// Display Future Forecast
+var displayForecast = function (data) {
+  // Day 1
+  document.getElementById('day-1-temp').textContent =
+    data.list[0].main.temp + ' ˚F';
+  document.getElementById('day-1-wind').textContent =
+    data.list[0].wind.speed + ' MPH';
+  document.getElementById('day-1-humidity').textContent =
+    data.list[0].main.humidity + ' %';
+  // Day 2
+  document.getElementById('day-2-temp').textContent =
+    data.list[1].main.temp + ' ˚F';
+  document.getElementById('day-2-wind').textContent =
+    data.list[1].wind.speed + ' MPH';
+  document.getElementById('day-2-humidity').textContent =
+    data.list[1].main.humidity + ' %';
+  // Day 3
+  document.getElementById('day-3-temp').textContent =
+    data.list[2].main.temp + ' ˚F';
+  document.getElementById('day-3-wind').textContent =
+    data.list[2].wind.speed + ' MPH';
+  document.getElementById('day-3-humidity').textContent =
+    data.list[2].main.humidity + ' %';
+  // Day 4
+  document.getElementById('day-4-temp').textContent =
+    data.list[3].main.temp + ' ˚F';
+  document.getElementById('day-4-wind').textContent =
+    data.list[3].wind.speed + ' MPH';
+  document.getElementById('day-4-humidity').textContent =
+    data.list[3].main.humidity + ' %';
+  // Day 5
+  document.getElementById('day-5-temp').textContent =
+    data.list[4].main.temp + ' ˚F';
+  document.getElementById('day-5-wind').textContent =
+    data.list[4].wind.speed + ' MPH';
+  document.getElementById('day-5-humidity').textContent =
+    data.list[4].main.humidity + ' %';
+};
+
+// console.log($('#cur-temp'));
+
+// Display Forecast Function
 
 // check recentCities array / localstorage and call return city weather function passing first item in array / local storage.
 
